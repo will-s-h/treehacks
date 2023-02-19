f222465518cc2c50f72d8d7793245abfcbfd1f09
will-s-h <107830908+will-s-h@users.noreply.github.com>
2023-02-19 02:04:35 -0800

    level structure

--- UltimatePygameIntro/edited.py
+++ UltimatePygameIntro/edited.py
@@ -1,9 +1,8 @@
 import pygame
 from sys import exit
-import text
+import levels
 import textbox
 from random import randint, choice
-
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -48,7 +47,7 @@ class Player(pygame.sprite.Sprite):
             info.append((obstacle.rect.x, obstacle.rect.y, obstacle.type))
 
         try:
-            jump = self.custom(info)
+            jump = self.custom(self.rect.x, self.rect.y, info)
         except:
             pass
         
@@ -107,7 +106,6 @@ class Obstacle(pygame.sprite.Sprite):
         if self.rect.x <= -100: 
             self.kill()
 
-
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
     score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
@@ -155,7 +153,6 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
-initial_screen = True
 game_active = False
 start_time = 0
 score = 0
@@ -223,96 +220,106 @@ pygame.time.set_timer(snail_animation_timer,500)
 fly_animation_timer = pygame.USEREVENT + 3
 pygame.time.set_timer(fly_animation_timer,200)
 
+# custom code
+pygame.key.set_repeat(500, 25)
 code = textbox.TextInputBox(x=10, y=10, font_family="monospace", font_size=20, max_width=780, max_height=380)
+level = 0
 
 while True:
-	events = pygame.event.get()
+    events = pygame.event.get()
 
-	# allows typing in text box
-	if not game_active and not initial_screen: 
-		code.update(events)
-	
-	for event in events:
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
-		
-		if game_active:
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
-					player_gravity = -20
-			
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-					player_gravity = -20
-                                        
-			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-				game_active = False
-			
-		else:
-			# click play button
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				if event.pos[0] in range(game_message_rect.centerx - game_message_rect.width // 2, game_message_rect.centerx + game_message_rect.width // 2) and \
-				   event.pos[1] in range(game_message_rect.centery - game_message_rect.height // 2, game_message_rect.centery + game_message_rect.height // 2):
-					game_active = True
-					start_time = int(pygame.time.get_ticks() / 1000)
-			player.update(obstacle_group, string=code.get_text())
+    # allows typing in text box
+    if not game_active and level != 0: 
+        code.update(events)
+    
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        # player jump / player force quit
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+                    player_gravity = -20
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+            
+            # force quit to next level                            
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                game_active = False
+                if level < len(levels.texts) - 1 and score >= levels.reqs[level]: level += 1
+            
+        else:
+            # click play button
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] in range(game_message_rect.centerx - game_message_rect.width // 2, game_message_rect.centerx + game_message_rect.width // 2) and \
+                   event.pos[1] in range(game_message_rect.centery - game_message_rect.height // 2, game_message_rect.centery + game_message_rect.height // 2):
+                    game_active = True
+                    start_time = int(pygame.time.get_ticks() / 1000)
+                    text_counter = 0
+                    player.update(obstacle_group, string=code.get_text())
+                    code.clear_text()
 
-		if game_active:
-			if event.type == obstacle_timer:
-				obstacle_group.add(Obstacle(choice(['fly','snail','snail','snail'])))
+        # add obstacles
+        if game_active:
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly','snail','snail','snail'])))
 
-			if event.type == snail_animation_timer:
-				if snail_frame_index == 0: snail_frame_index = 1
-				else: snail_frame_index = 0
-				snail_surf = snail_frames[snail_frame_index] 
+            if event.type == snail_animation_timer:
+                if snail_frame_index == 0: snail_frame_index = 1
+                else: snail_frame_index = 0
+                snail_surf = snail_frames[snail_frame_index] 
 
-			if event.type == fly_animation_timer:
-				if fly_frame_index == 0: fly_frame_index = 1
-				else: fly_frame_index = 0
-				fly_surf = fly_frames[fly_frame_index] 
+            if event.type == fly_animation_timer:
+                if fly_frame_index == 0: fly_frame_index = 1
+                else: fly_frame_index = 0
+                fly_surf = fly_frames[fly_frame_index] 
 
 
-	if game_active:
-		initial_screen = False
-		screen.blit(sky_surface,(0,0))
-		screen.blit(ground_surface,(0,300))
-		score = display_score()
-		
-		player.draw(screen)
-		player.update(obstacle_group)
+    if game_active:
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,(0,300))
+        score = display_score()
+        
+        player.draw(screen)
+        player.update(obstacle_group)
 
-		obstacle_group.draw(screen)
-		obstacle_group.update()
+        obstacle_group.draw(screen)
+        obstacle_group.update()
 
-		# collision 
-		game_active = collision_sprite()
-		
-	else:
-		screen.fill((94,129,162))
-		if initial_screen:
-			screen.blit(player_stand,player_stand_rect)
+        # collision detection
+        game_active = collision_sprite()
+        if not game_active: # if killed, move to next level
+            if level < len(levels.texts) - 1 and score >= levels.reqs[level]: level += 1
+        
+    else:
+        screen.fill((94,129,162))
+        if level == 0:
+            screen.blit(player_stand, player_stand_rect)
 
-			score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
-			score_message_rect = score_message.get_rect(center = (400,330))
-			screen.blit(game_name,game_name_rect)
+            score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
+            score_message_rect = score_message.get_rect(center = (400,330))
+            screen.blit(game_name,game_name_rect)
+            screen.blit(game_message,game_message_rect)
+        else:
+            if len(levels.texts[level]) > text_counter: 
+                pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_c, unicode=levels.texts[level][text_counter]))
+                text_counter += 1
 
-			screen.blit(game_message,game_message_rect)
-			# pygame.draw.rect(screen, (111,196,169), game_message_rect, 2)
-		else:
-			if len(text.text1) > text_counter: 
-				pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_c, unicode=text.text1[text_counter]))
-				text_counter += 1
-			# display text
-			code.render(screen)
-                        
-			game_message = test_font.render('PLAY',False,(111,196,169))
-			game_message_rect = game_message.get_rect(center = (750,370))
-			screen.blit(game_message,game_message_rect)
+            # display code
+            code.render(screen)
+            
+            # new location for play button for level >= 0
+            game_message = test_font.render('PLAY',False,(111,196,169))
+            game_message_rect = game_message.get_rect(center = (750,370))
+            screen.blit(game_message, game_message_rect)
 
-		obstacle_rect_list.clear()
-		player_rect.midbottom = (80,300)
-		player_gravity = 0
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
 
-	pygame.display.update()
-	clock.tick(60)
+    pygame.display.update()
+    clock.tick(60)
--- UltimatePygameIntro/text.py
+++
@@ -1,1 +0,0 @@
-text1 = "Good job on your first run. You did good, but there is a better way to do this. Let's try writing some code to help out.\n\n"
