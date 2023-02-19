92b2fce09eb639d127ad2938d3a36d30193682e4
Sheden Andemicael <shedenandemicael@DNa1de864.SUNet>
2023-02-18 17:12:59 -0800

    changing menus

--- UltimatePygameIntro/edited.py
+++ UltimatePygameIntro/edited.py
@@ -1,6 +1,9 @@
 import pygame
 from sys import exit
+import text
+import textbox
 from random import randint, choice
+
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
@@ -135,9 +138,11 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
+initial_screen = True
 game_active = False
 start_time = 0
 score = 0
+text_counter = 0
 bg_music = pygame.mixer.Sound('./audio/music.wav')
 bg_music.play(loops = -1)
 
@@ -188,7 +193,7 @@ player_stand_rect = player_stand.get_rect(center = (400,200))
 game_name = test_font.render('Pixel Runner',False,(111,196,169))
 game_name_rect = game_name.get_rect(center = (400,80))
 
-game_message = test_font.render('Press esc to run',False,(111,196,169))
+game_message = test_font.render('PLAY',False,(111,196,169))
 game_message_rect = game_message.get_rect(center = (400,330))
 
 # Timer 
@@ -201,12 +206,14 @@ pygame.time.set_timer(snail_animation_timer,500)
 fly_animation_timer = pygame.USEREVENT + 3
 pygame.time.set_timer(fly_animation_timer,200)
 
-import textbox
 code = textbox.TextInputBox(x=10, y=10, font_family="monospace", font_size=20, max_width=780, max_height=380)
 
 while True:
 	events = pygame.event.get()
-	if not game_active: code.update(events)
+
+	# allows typing in text box
+	if not game_active and not initial_screen: 
+		code.update(events)
 	
 	for event in events:
 		if event.type == pygame.QUIT:
@@ -223,13 +230,16 @@ while True:
 					player_gravity = -20
 			
 		else:
-			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-				game_active = True
-				start_time = int(pygame.time.get_ticks() / 1000)
+			# click play button
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.pos[0] in range(game_message_rect.centerx - game_message_rect.width // 2, game_message_rect.centerx + game_message_rect.width // 2) and \
+				   event.pos[1] in range(game_message_rect.centery - game_message_rect.height // 2, game_message_rect.centery + game_message_rect.height // 2):
+					game_active = True
+					start_time = int(pygame.time.get_ticks() / 1000)
 
 		if game_active:
 			if event.type == obstacle_timer:
-				obstacle_group.add(Obstacle(choice(['fly','snail','snail','snail'])))
+				obstacle_group.add(Obstacle(choice(['fly','fly','fly','fly'])))
 
 			if event.type == snail_animation_timer:
 				if snail_frame_index == 0: snail_frame_index = 1
@@ -243,6 +253,7 @@ while True:
 
 
 	if game_active:
+		initial_screen = False
 		screen.blit(sky_surface,(0,0))
 		screen.blit(ground_surface,(0,300))
 		score = display_score()
@@ -258,21 +269,25 @@ while True:
 		
 	else:
 		screen.fill((94,129,162))
-		screen.blit(player_stand,player_stand_rect)
-		
-		# display text
-		code.render(screen)
-  
+		if initial_screen:
+			screen.blit(player_stand,player_stand_rect)
+
+			score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
+			score_message_rect = score_message.get_rect(center = (400,330))
+			screen.blit(game_name,game_name_rect)
+
+			screen.blit(game_message,game_message_rect)
+			pygame.draw.rect(screen, (111,196,169), game_message_rect, 2)
+		else:
+			if len(text.text1) > text_counter: 
+				pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_c, unicode=text.text1[text_counter]))
+				text_counter += 1
+			# display text
+			code.render(screen)
+
 		obstacle_rect_list.clear()
 		player_rect.midbottom = (80,300)
 		player_gravity = 0
-
-		score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
-		score_message_rect = score_message.get_rect(center = (400,330))
-		screen.blit(game_name,game_name_rect)
-
-		if score == 0: screen.blit(game_message,game_message_rect)
-		else: screen.blit(score_message,score_message_rect)
 
 	pygame.display.update()
 	clock.tick(60)
